36751a99171be0755f800d052efd8a26b1932b1b
Abhinav Srivastava <abhinavsrivastava2611@gmail.com>
2025-06-23 15:49:03 +0530

    Update the code make it better

--- Numbers/ReverseNumber.java
+++ Numbers/ReverseNumber.java
@@ -1,15 +1,37 @@
 package Numbers;
 
+import java.util.Scanner;
+
 public class ReverseNumber {
     public static void main(String[] args) {
-        int n = 1234;
-        int rev =0;
-        while (n!=0) {
-            int digit = n%10;
-            rev = rev*10+digit;
-            n=n/10;
+        Scanner sc = new Scanner(System.in); 
+
+         System.out.println("Enter the number!");
+        // Check if input is not a number or whether the input value exceeds the range of the written datatype
+        if(!sc.hasNextLong())
+        {
+            System.out.println("Enter valid number");
+            sc.close();
+            return;
         }
 
-        System.out.println(rev);
+        long n = sc.nextLong();
+        System.out.println("original number"+ n);
+     
+        long rev =0;
+        long number = Math.abs(n);
+        while (number!=0) {
+            long digit = number%10;
+            rev = rev*10+digit;
+            number=number/10;
+        }
+
+      if(n<0)
+      {
+        rev = -rev;
+      }
+      System.out.println("Reversed Number: "+rev);
+      sc.close();
     }
+
 }
