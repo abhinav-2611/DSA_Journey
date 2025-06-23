ed42662502334cb5a7133e462b131c050b9dd62c
Abhinav Srivastava <abhinavsrivastava2611@gmail.com>
2025-06-23 23:03:29 +0530

    initial commit

---
+++ Numbers/primeNumber.java
@@ -0,0 +1,34 @@
+package Numbers;
+
+import java.util.Scanner;
+
+public class primeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your number:");
+        if(!sc.hasNextInt())
+        {
+          System.out.println("enter valid value!");
+        }
+        int num = sc.nextInt();
+        boolean isPrime = true;
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i<=Math.sqrt(num); i++) {
+
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        if (isPrime) {
+            System.out.println(num + " is a Prime number.");
+        } else {
+            System.out.println(num + " is NOT a Prime number.");
+        }
+
+        sc.close();
+    }
+}
